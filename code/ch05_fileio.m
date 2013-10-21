e6e47f0f3eb2e846c3f736fa2600bab6a7934b16
KEClaytor <KE.Claytor@gmail.com>
2013-10-21 18:18:24 -0400

    Changed around file IO a little bit. Removed low-level example and put in user inputs / menus.

--- code/ch05_fileio.m
+++ code/ch05_fileio.m
@@ -1,28 +1,34 @@
+%% Get user inputs
+% Input gets input off of the screen
+x = input('Give me a number: ')
+% You can pop-up a display box
+y = inputdlg('Give me a name:','Name',1)
+% or even a menu
+choice = menu('So many choices!','Choice A','Choice B','Choice C')
+
+%% Read in a file
 % Read in a comma separated text file.
 % Operate on it and save the new results
 
 % Build a filepath based on the current directory
 fname = fullfile(pwd,'ch05_spydr.csv');
 
-%% Loading with basic fscanf
-% This section is currently a total mess
-fid = fopen(fname);
-ncol = 7;
-titles = cell(1,ncol);
-for ii = 1:ncol
-    titles{ii} = fscanf(fid,'%s,');
-end
-A = fscanf(fid,['%s',repmat('%f',[1,7])]);
-fclose(fid)
-
-%% Loading with higher-leve, IO
-% This took me a fraction of the time to write
+% Loading with high-lever, IO
 filedata = importdata(fname);
 A = filedata.data;
 titles = filedata.textdata(1,:);
 times = filedata.textdata(2:end,:);
 
-%% Saving the data
+%% Saving data
+% Make some fake data to save;
+matrix_titles = {'Column A','Col B','Col C','Col 4'};
+matrix_data = rand(4);
+out_name = 'ch05_output';
 
-% Because someone asks for it in excel...
+% Saving data using csv
+csvwrite(out_name,matrix_data);
 
+% Saving Data as Excel
+% (because someone asks for it in excel...)
+xlswrite(out_name,matrix_titles,'Sheet1','A1');
+xlswrite(out_name,matrix_data,'Sheet1','A2');
